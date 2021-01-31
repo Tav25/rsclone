8d9de2b9ddd0e9a6bfd5dfb2a6faea20ce2bde27
Tav25 <turbin28@gmail.com>
2021-01-31 17:23:49 +0300

    feat: inventory walkthrough

--- src/View/Inventory.js
+++ src/View/Inventory.js
@@ -57,17 +57,20 @@ class Inventory extends Phaser.GameObjects.Container {
   }
 
   objectPositionInTheList3() {
+    let i = 0
     this.scene.model.world.mainCharacter.inventory.itemList.forEach((e) => {
       console.log('E:', e.index, e);
-      const imagePositionY = 18 + 32 * e.range;
-      const textPositionY = 7 + 32 * e.range;
+      const imagePositionY = 18 + 32 * i;
+      const textPositionY = 7 + 32 * i;
+      i=+1
 
       const imageInInventory = this.scene.add.image(18, imagePositionY, 'atlasPersonsObject', e.icon);
       this.add(imageInInventory);
       imageInInventory.setInteractive();
 
       imageInInventory.on('pointerdown', (pointer) => {
-        this.scene.world.mainCharacter.equipment.equipWeapon(e)
+        this.scene.model.world.mainCharacter.equipment.equipWeapon(e)
+        console.log(this.scene.model.world.mainCharacter)
       });
 
       const textInInventory = this.scene.add.text(42, textPositionY, '', {});
